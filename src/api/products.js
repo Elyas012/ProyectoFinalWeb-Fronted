189c1094f822e8cf9dd6df29c06c6492049e8522
Elias Jacome <eliasjacome77@gmail.com>
2025-08-05 21:01:24 -0500

    fronted2

--- src/api/products.js
+++ src/api/products.js
@@ -1,8 +1,10 @@
 // client/src/api/products.js
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
+const baseUrl = API_URL.endsWith('/') ? API_URL.slice(0, -1) : API_URL;
 
 export const fetchProducts = async () => {
-  const res = await fetch(`${API_URL}/products`);
+  const res = await fetch(`${baseUrl}/products`);
   if (!res.ok) throw new Error('Error al cargar productos');
   return res.json();
 };
+
